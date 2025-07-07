eefee06c370914e282a45d566c82b40ab24a514b
andersondario-er <andersona@earthranger.com>
2025-07-07 09:05:34 -0300

    Conditional data object

--- terraform/modules/cloud-run.tf
+++ terraform/modules/cloud-run.tf
@@ -60,6 +60,7 @@ resource "google_cloud_run_v2_service" "default" {
 }
 
 data "google_dns_managed_zone" "ecoscope" {
+  count   = startswith(var.env, "dev-preview") ? 1 : 0
   name    = var.dns_config_name
   project = var.project_id
 }
@@ -67,7 +68,7 @@ data "google_dns_managed_zone" "ecoscope" {
 resource "google_dns_record_set" "cname" {
   count        = startswith(var.env, "dev-preview") ? 1 : 0
   name         = "${var.service_url}."
-  managed_zone = data.google_dns_managed_zone.ecoscope.name
+  managed_zone = data.google_dns_managed_zone.ecoscope[0].name
   type         = "CNAME"
   ttl          = 300
   rrdatas      = ["ghs.googlehosted.com."]
